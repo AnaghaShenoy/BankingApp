3ec8f2f7db4a74213cf2f4afd8774f5b14bf4951
Anagha Shenoy <anaghashenoyr@gmail.com>
2024-08-24 19:45:48 +0530

    small changes in AccountDto

--- banking-app/src/main/java/com/learning/banking/dto/AccountDto.java
+++ banking-app/src/main/java/com/learning/banking/dto/AccountDto.java
@@ -8,16 +8,16 @@ import lombok.Data;
 @AllArgsConstructor
 // this provides constructors
 public class AccountDto {
-//    private Long id;
-//    private String accountHolderName;
-//    private double balance;
-    @NotNull(message = "Account number cannot be null")
-    private Long accountNumber;
-
-    @NotEmpty(message = "Account holder name cannot be empty")
+    private Long id;
     private String accountHolderName;
-
-    @Min(value = 0, message = "Balance must be positive")
-    private Double balance;
+    private double balance;
+//    @NotNull(message = "Account number cannot be null")
+//    private Long accountNumber;
+//
+//    @NotEmpty(message = "Account holder name cannot be empty")
+//    private String accountHolderName;
+//
+//    @Min(value = 0, message = "Balance must be positive")
+//    private Double balance;
 
 }
