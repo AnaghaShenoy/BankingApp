89370eca72e8d7030593d2da7320c91720790e56
Anagha Shenoy <anaghashenoyr@gmail.com>
2024-08-24 10:56:07 +0530

    successfully build get account REST API - GET METHOD IMPLEMENTATION

--- banking-app/src/main/java/com/learning/banking/controller/AccountController.java
+++ banking-app/src/main/java/com/learning/banking/controller/AccountController.java
@@ -4,10 +4,7 @@ import com.learning.banking.dto.AccountDto;
 import com.learning.banking.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 // make this class as a spring MVC Rest controller
@@ -28,5 +25,15 @@ public class AccountController {
         // @RequestBody - maps request body to AccountDto object (java object)
         // @RequestBody contains json and it auto converts json to java
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
+    }
+
+    // Get Account REST API
+    // method returns the account obj and ok method internally returns the HTTP status 200 OK
+    @GetMapping("/{id}") // to handle HTTP GET requests | maps the incoming HTTP GET request to this method
+    // the {id} in the annotation is the template variable
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id) {
+        // @PathVariable is used to bind template path variable to argument
+        AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
     }
 }
